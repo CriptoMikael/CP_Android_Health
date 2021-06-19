566a673ce640449a8085c953f5c7be82574aeea0
CriptoMikael <mikhailin98@yandex.ru>
2021-06-19 03:27:04 +0300

    Add voice recognize speech

--- app/src/main/java/com/example/sharpobject/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/sharpobject/ui/home/HomeFragment.java
@@ -1,9 +1,16 @@
 package com.example.sharpobject.ui.home;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -18,14 +25,22 @@ import com.example.sharpobject.databinding.FragmentHomeBinding;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import static android.app.Activity.RESULT_OK;
 
 public class HomeFragment extends Fragment {
 
+    private static final int REQ_CODE_SPEECH_INPUT = 100;
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
     private View rootView;
+    private static Button speakButton;
+    private EditText mVoiceInputTv;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -33,6 +48,16 @@ public class HomeFragment extends Fragment {
                 new ViewModelProvider(this).get(HomeViewModel.class);
 
         rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        speakButton = (Button) rootView.findViewById(R.id.speakButton);
+        mVoiceInputTv = (EditText) rootView.findViewById(R.id.editTextTextPersonName4);
+        speakButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startVoiceInput();
+            }
+        });
+
         EditText textDate = rootView.findViewById(R.id.editTextDate2);
         Date datedate = Calendar.getInstance().getTime();
         DateFormat dateFormat = new SimpleDateFormat("hh:mm dd-MM-yyyy");
@@ -40,6 +65,34 @@ public class HomeFragment extends Fragment {
         textDate.setText(date);
 
         return rootView;
+    }
+
+
+    private void startVoiceInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hello, How can I help you?");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            System.out.println(a);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    mVoiceInputTv.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
     }
 
     @Override
