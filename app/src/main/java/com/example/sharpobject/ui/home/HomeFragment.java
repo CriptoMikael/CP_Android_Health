5e05c3decb5a8f2f311d77d662e8dab6e8733b49
CriptoMikael <mikhailin98@yandex.ru>
2021-06-19 14:57:08 +0300

    Added fill fields from voices, create JSONObject with values

--- app/src/main/java/com/example/sharpobject/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/sharpobject/ui/home/HomeFragment.java
@@ -2,8 +2,10 @@ package com.example.sharpobject.ui.home;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,19 +13,27 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.sharpobject.R;
 import com.example.sharpobject.databinding.FragmentHomeBinding;
 
-import java.sql.Struct;
+import org.json.JSONObject;
+
+import java.net.URL;
+import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
+import java.util.TimeZone;
 
 import static android.app.Activity.RESULT_OK;
 
@@ -34,6 +44,7 @@ public class HomeFragment extends Fragment {
     private FragmentHomeBinding binding;
     private View rootView;
     private static Button speakButton;
+    private static Button sendButton;
     private EditText dateField;
     private EditText modelName;
     private EditText highValue;
@@ -42,6 +53,12 @@ public class HomeFragment extends Fragment {
     private EditText activity;
     private EditText mVoiceInputTv;
 
+    private Long timeStamp;
+    private String modelString;
+    private int highString;
+    private int lowString;
+    private int pulseString;
+    private String activityString;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -57,11 +74,41 @@ public class HomeFragment extends Fragment {
         lowValue = (EditText) rootView.findViewById(R.id.lowValue);
         pulse = (EditText) rootView.findViewById(R.id.pulse);
         activity = (EditText) rootView.findViewById(R.id.activity);
+
+        mVoiceInputTv.setVisibility(View.INVISIBLE);
+        dateField.setVisibility(View.INVISIBLE);
+        modelName.setVisibility(View.INVISIBLE);
+        highValue.setVisibility(View.INVISIBLE);
+        lowValue.setVisibility(View.INVISIBLE);
+        pulse.setVisibility(View.INVISIBLE);
+        activity.setVisibility(View.INVISIBLE);
+
+        timeStamp = 0L;
+        modelString = new String("");
+        highString = 0;
+        lowString = 0;
+        pulseString = 0;
+        activityString = new String("");
+
+        highValue.setText("0");
+        lowValue.setText("0");
+        pulse.setText("0");
+        activity.setText("");
+
         speakButton = (Button) rootView.findViewById(R.id.speakButton);
         speakButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startVoiceInput();
+            }
+        });
+
+        sendButton = (Button) rootView.findViewById(R.id.sendButton);
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                sendAllData();
             }
         });
 
@@ -86,11 +133,19 @@ public class HomeFragment extends Fragment {
         switch (requestCode) {
             case REQ_CODE_SPEECH_INPUT: {
                 if (resultCode == RESULT_OK && null != data) {
+                    dateField.setVisibility(View.VISIBLE);
+                    modelName.setVisibility(View.VISIBLE);
+                    highValue.setVisibility(View.VISIBLE);
+                    lowValue.setVisibility(View.VISIBLE);
+                    pulse.setVisibility(View.VISIBLE);
+                    activity.setVisibility(View.VISIBLE);
+
                     ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     parseValue(result.get(0));
                     mVoiceInputTv.setText(result.get(0));
                     Date datedate = Calendar.getInstance().getTime();
-                    DateFormat dateFormat = new SimpleDateFormat("hh:mm dd-MM-yyyy");
+                    timeStamp = datedate.getTime();
+                    DateFormat dateFormat = new SimpleDateFormat("kk:mm dd-MM-yyyy");
                     String date = dateFormat.format(datedate);
                     dateField.setText(date);
                     modelName.setText("Omron M2 Classic");
@@ -130,11 +185,11 @@ public class HomeFragment extends Fragment {
                     }
                 }
                 else {
-                    highValue.setText(high);
-                    lowValue.setText(low);
                     break;
                 }
             }
+            highValue.setText(high);
+            lowValue.setText(low);
         }
         if (dataString.contains("пульс")) {
             boolean flag = false;
@@ -158,6 +213,36 @@ public class HomeFragment extends Fragment {
         }
         if (i < dataString.length()) {
             activity.setText(dataString.substring(i));
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void sendAllData() {
+        try {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("kk:mm dd-MM-yyyy");
+            Date parsedDate = dateFormat.parse(dateField.getText().toString());
+            Timestamp timestamp = new Timestamp(parsedDate.getTime());
+            timeStamp = timestamp.getTime();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+        modelString = modelName.getText().toString();
+        highString = Integer.parseInt(highValue.getText().toString());
+        lowString = Integer.parseInt(lowValue.getText().toString());
+        pulseString = Integer.parseInt(pulse.getText().toString());
+        activityString = activity.getText().toString();
+
+        JSONObject json = new JSONObject();
+        try {
+            json.put("date", timeStamp);
+            json.put("modelName", modelString);
+            json.put("highValue", highString);
+            json.put("lowValue", lowString);
+            json.put("pulse", pulseString);
+            json.put("activity", activityString);
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
 
